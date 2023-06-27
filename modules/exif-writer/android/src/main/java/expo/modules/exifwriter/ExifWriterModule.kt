5eaa538a35be518835140bfa5664c3a4f7f3c009
BBudach <BBudach@smumn.edu>
2023-06-27 14:06:47 -0500

    updated exif-writer module to remove unnecessary functions

--- modules/exif-writer/android/src/main/java/expo/modules/exifwriter/ExifWriterModule.kt
+++ modules/exif-writer/android/src/main/java/expo/modules/exifwriter/ExifWriterModule.kt
@@ -17,36 +17,9 @@ class ExifWriterModule : Module() {
     // The module will be accessible from `requireNativeModule('ExifWriter')` in JavaScript.
     Name("ExifWriter")
 
-    // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
-    Constants(
-      "PI" to Math.PI
-    )
-
-    // Defines event names that the module can send to JavaScript.
-    Events("onChange")
-
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
     Function("writeExif") { uri: String, latitude : Double, longitude : Double, altitude : Double ->
       writeLocationExif(uri, latitude, longitude, altitude);
       "exif saved"
-    }
-
-    // Defines a JavaScript function that always returns a Promise and whose native code
-    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("setValueAsync") { value: String ->
-      // Send an event to JavaScript.
-      sendEvent("onChange", mapOf(
-        "value" to value
-      ))
-    }
-
-    // Enables the module to be used as a native view. Definition components that are accepted as part of
-    // the view definition: Prop, Events.
-    View(ExifWriterView::class) {
-      // Defines a setter for the `name` prop.
-      Prop("name") { view: ExifWriterView, prop: String ->
-        println(prop)
-      }
     }
   }
 
--- modules/exif-writer/index.ts
+++ modules/exif-writer/index.ts
@@ -3,25 +3,8 @@ import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-cor
 // Import the native module. On web, it will be resolved to ExifWriter.web.ts
 // and on native platforms to ExifWriter.ts
 import ExifWriterModule from './src/ExifWriterModule';
-import ExifWriterView from './src/ExifWriterView';
-import { ChangeEventPayload, ExifWriterViewProps } from './src/ExifWriter.types';
 import { Double } from 'react-native/Libraries/Types/CodegenTypes';
-
-// Get the native constant value.
-export const PI = ExifWriterModule.PI;
 
 export function writeExif(uri: string, latitude : Double, longitude : Double, altitude : Double): string {
   return ExifWriterModule.writeExif(uri, latitude, longitude, altitude);
 }
-
-export async function setValueAsync(value: string) {
-  return await ExifWriterModule.setValueAsync(value);
-}
-
-const emitter = new EventEmitter(ExifWriterModule ?? NativeModulesProxy.ExifWriter);
-
-export function addChangeListener(listener: (event: ChangeEventPayload) => void): Subscription {
-  return emitter.addListener<ChangeEventPayload>('onChange', listener);
-}
-
-export { ExifWriterView, ExifWriterViewProps, ChangeEventPayload };
